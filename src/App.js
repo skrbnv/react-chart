cfb612953c259895e2ee73c92ef0434266260daa
Sergey Skrebnev <sergey.skrebnev@gmail.com>
2023-03-10 21:30:14 +0300

    company via query string

--- src/App.js
+++ src/App.js
@@ -9,15 +9,18 @@ console.log(data2)
 class App extends React.Component {
   constructor(props) {
     super(props);
+    let search = window.location.search;
+    let params = new URLSearchParams(search);  
     this.state = {
       error: null,
+      company: params.get('company'),
       isLoaded: false,
       data: {'edges': [], 'nodes': []}
     };
   }
 
   componentDidMount() {
-    fetch("https://data-scripts-ednprdella-uc.a.run.app/data/company/structure/1")
+    fetch("https://data-scripts-ednprdella-uc.a.run.app/data/company/structure/"+this.state.company)
       .then(res => res.json())
       .then(
         (result) => {
