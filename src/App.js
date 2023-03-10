7571de70fee5c6f79635e926ce4bd5722ae0df85
Sergey Skrebnev <sergey.skrebnev@gmail.com>
2023-03-10 18:16:08 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
+import Graphin, { Behaviors, Utils } from '@antv/graphin';
+import { Row, Col, Card } from 'antd';
 import './App.css';
+const { ZoomCanvas, FitView } = Behaviors;
+const data2 = Utils.mock(8).tree().graphin();
+console.log(data2)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: {'edges': [], 'nodes': []}
+    };
+  }
 
-export default App;
+  componentDidMount() {
+    fetch("http://0.0.0.0:5000/data/company/structure/1")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            data: result
+          });
+          console.log(result)
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    return (
+      <div className='viewport'>
+        {this.state.isLoaded ? (
+          <Graphin data={this.state.data} layout={{ type: 'graphin-force' }}>
+              <ZoomCanvas enabled />
+              <FitView />
+          </Graphin>
+        ) : (
+          <p>Loading..</p>
+        )
+        }
+      </div>
+  )};
+};
+
+export default App
